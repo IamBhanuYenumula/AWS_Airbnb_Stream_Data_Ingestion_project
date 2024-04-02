bb0488590011962ba14195c4d199846a408bd18b
iambhanuprakash <130715988+iambhanuprakash@users.noreply.github.com>
2024-04-01 23:41:43 -0700

    incrimental_save

--- save_to_s3/lambda_function.py
+++ save_to_s3/lambda_function.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import io
 import pandas as pd
 import os
+import uuid
 # from dotenv import load_dotenv
 
 # load_dotenv()
@@ -13,13 +14,14 @@ def lambda_handler(event, context):
     s3_client = boto3.client('s3')
     s3_resource = boto3.resource('s3')
     today_date = str(datetime.today())
+    uniqueID = str(uuid.uuid4())
     print("Event: ", event)
     message = event[0]['message']
     
     if (message == {}):
         return {}
     try:
-        obj = s3_client.get_object(Bucket = "airbnb-data-from-lambda", Key = f"date={today_date}/{today_date}.csv")
+        obj = s3_client.get_object(Bucket = "airbnb-data-from-lambda", Key = f"date={today_date}/{today_date}_{uniqueID}.csv")
         obj = obj['body'].read()
         obj_str = str(obj, 'utf-8')
         data = io.StringIO(obj_str)
@@ -46,7 +48,7 @@ def lambda_handler(event, context):
                                 message["price"],
                                 message["totalDays"]]
         df.to_csv('/tmp/test.csv', encoding = 'utf-8')
-        s3_resource.Bucket("airbnb-data-from-lambda").upload_file('/tmp/test.csv',f"date={today_date}/{today_date}.csv")
+        s3_resource.Bucket("airbnb-data-from-lambda").upload_file('/tmp/test.csv',f"date={today_date}/{today_date}_{uniqueID}.csv")
         
     
     return {
